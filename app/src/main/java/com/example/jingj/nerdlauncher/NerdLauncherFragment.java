b93f3cd3cb1fd3f8086f6427f39ed02e0709e0d1
reply-1988 <reply-1988@users.noreply.github.com>
2018-05-06 18:45:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jingj/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,133 @@
+package com.example.jingj.nerdlauncher;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
+import android.media.Image;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class NerdLauncherFragment extends Fragment {
+
+    private static final String TAG = "NerdLauncherFragment";
+
+    private RecyclerView mRecyclerView;
+
+    public static NerdLauncherFragment newInstance() {
+        return new NerdLauncherFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_nerd_launcher, container, false);
+        mRecyclerView = v.findViewById(R.id.app_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        setupAdapter();
+        return v;
+    }
+
+    private void setupAdapter() {
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        //此处不适用startActivity方法是因为其启动的是匹配隐式Intent的默认activity
+        //使用getActivity方法会给intent添加一个Intent.CategoryDefault类别，而这个类别不是每个程序都有的
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+        //将ResolveInfo对象中的activity标签按照首字母排序
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo o1, ResolveInfo o2) {
+                PackageManager pm = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                        o1.loadLabel(pm).toString(),
+                        o2.loadLabel(pm).toString());
+            }
+        });
+
+        Log.i(TAG, "Found " + activities.size() + " activities.");
+        mRecyclerView.setAdapter(new ActivityAdapter(activities));
+    }
+
+    private class ActivityHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        private ResolveInfo mResolveInfo;
+        private TextView mNameTextView;
+        private ImageView mImageView;
+
+        public ActivityHolder(View itemView) {
+            super(itemView);
+            mNameTextView = itemView.findViewById(R.id.app_title);
+            mImageView = itemView.findViewById(R.id.app_pic);
+            mNameTextView.setOnClickListener(this);
+        }
+
+        public void bindActivity(ResolveInfo resolveinfo) {
+            mResolveInfo = resolveinfo;
+            PackageManager pm = getActivity().getPackageManager();
+            String appName = mResolveInfo.loadLabel(pm).toString();
+            Drawable appImage = mResolveInfo.loadIcon(pm);
+            mNameTextView.setText(appName);
+            mImageView.setImageDrawable(appImage);
+        }
+
+        @Override
+        public void onClick(View v) {
+            ActivityInfo activityInfo = mResolveInfo.activityInfo;
+
+            Intent i = new Intent(Intent.ACTION_MAIN)
+                    .setClassName(activityInfo.applicationInfo.packageName, activityInfo.name)
+                    //使应用从一个新的Task中打开
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            startActivity(i);
+        }
+    }
+
+    private class ActivityAdapter extends RecyclerView.Adapter<ActivityHolder> {
+
+        private final List<ResolveInfo> mActivities;
+
+        private ActivityAdapter(List<ResolveInfo> activities) {
+            mActivities = activities;
+        }
+
+        @NonNull
+        @Override
+        public ActivityHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(R.layout.text_and_picture, parent, false);
+            return new ActivityHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ActivityHolder holder, int position) {
+            ResolveInfo resolveInfo = mActivities.get(position);
+            holder.bindActivity(resolveInfo);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mActivities.size();
+        }
+    }
+}
